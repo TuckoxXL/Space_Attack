98a450aecc206b85fba462a123c58032cbcb56af
Your Name <sebastianbelluccini@gmail.com>
2023-11-14 20:12:42 -0300

    arreglos

--- Assets/Script/PowerUp/Factory.cs
+++ Assets/Script/PowerUp/Factory.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 
 public class Factory : MonoBehaviour
 {
-    
+    [SerializeField] private PowerUp[] skills;
+    private Dictionary<string, PowerUp> skillsByName;
+
+    private void Awake()
+    {
+        skillsByName = new Dictionary<string, PowerUp>(); foreach (var skill in skills)
+        {
+            skillsByName.Add(skill.skillName, skill);
+        }
+    }
+
+    public PowerUp CreatePowerup(string skillName, Transform playerTransform)
+    {
+        if (skillsByName.TryGetValue(skillName, out PowerUp skillPrefab))
+        {
+            PowerUp skillInstance = Instantiate(skillPrefab, playerTransform.position, Quaternion.identity);
+            return skillInstance;
+        }
+        else
+        {
+            Debug.LogWarning($"The ability '{skillName}' does not exist in the skills database.");
+            return null;
+        }
+    }
 }
