04b7bd9aefe0bf6c199a25f7d29069160674792d
TuckoxXL <sebastianbelluccini@gmail.com>
2023-11-19 22:56:31 -0300

    .

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    public float distance;
+    public int damage;
+
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void Update()
+    {
+
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("Enemy get shooted");
+            gameObject.SetActive(false);
+
+        }
+    }
+}
